0d19921c49d250ab839c5d3de488c13449d9d33a
Narwhalssss <114067296+Narwhalssss@users.noreply.github.com>
2023-01-10 11:13:39 +0700

    Update modal.js

--- modal.js
+++ modal.js
@@ -1,10 +1,13 @@
 var modal = $('#interest-modal');
+var gamers = $('#game-article1');
+var gamer = $('#game-article2');
 $('.box').hide();
 
 
 $('#container1').click(function() {
     modal.hide(1000);
-
+    gamers.show(1000);
+    gamer.show(1000);
 });
 
 $('#container2').click(function() {
@@ -17,3 +20,6 @@ $('#container3').click(function() {
 
 });
 
+gamer.click(function() {
+    window.location = 'https://clutchpoints.com/csgos-most-expensive-ak-47-skin-will-sell-for-over-400000-and-heres-why';
+})
