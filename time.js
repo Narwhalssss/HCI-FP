69a05453b69b6d9cc16379b1b81055b00854f4e5
Narwhalssss <114067296+Narwhalssss@users.noreply.github.com>
2023-01-10 11:13:08 +0700

    Update time.js

--- time.js
+++ time.js
@@ -1,10 +1,16 @@
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 function updateDate() {
     var currentDate = new Date();
     var date = currentDate.getDate();
     var month = currentDate.getMonth() + 1; // January is 0
     var year = currentDate.getFullYear();
+    var day = currentDate.getDay();
 
-    document.getElementById("current_date").innerHTML = ": " + date + "/" + month + "/" + year;
+    var element = document.getElementById("current_date");
+
+    // Update the text of the element with the ID "current_date"
+    element.innerHTML = ": " + days[day] + " " + date + "/" + month + "/" + year;
   }
 
   // Update the date every 1 second
