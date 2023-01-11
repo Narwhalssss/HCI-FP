38d946ed892c9d75f464e78fd70d8ead36083f36
Narwhalssss <114067296+Narwhalssss@users.noreply.github.com>
2023-01-11 19:46:41 +0700

    Add files via upload

---
+++ FInished-Project/modal.js
@@ -0,0 +1,76 @@
+var modal = $('#interest-modal');
+var gamers = $('#game-article1');
+var gamer = $('#game-article2');
+var game = $('#game-article3');
+var econ = $('#econ-article1');
+var econo = $('#econ-article2');
+var econom = $('#econ-article3');
+var tech = $('#tech-article1');
+var tech = $('#tech-article1');
+var techn = $('#tech-article2');
+var techno = $('#tech-article3');
+$('.box').hide();
+
+
+$('#container1').click(function() {
+    modal.hide(1000);
+    gamers.show(1000);
+    gamer.show(1000);
+    game.show(1000);
+});
+
+$('#container2').click(function() {
+    modal.hide(1000);
+    econ.show(1000);
+    econo.show(1000);
+    econom.show(1000);
+
+});
+
+$('#container3').click(function() {
+    modal.hide(1000);
+    tech.show(1000);
+    techn.show(1000);
+    techno.show(1000);
+});
+
+gamer.click(function() {
+    window.location = 'https://clutchpoints.com/csgos-most-expensive-ak-47-skin-will-sell-for-over-400000-and-heres-why';
+})
+
+gamers.click(function() {
+    window.location = 'https://www.gamespot.com/gallery/the-biggest-games-of-january-2023-forspoken-dead-space-and-more/2900-4546/';
+})
+
+game.click(function() {
+    window.location = 'https://www.gameshub.com/news/news/naughty-dog-playstation-new-game-release-date-announcements-37718/';
+})
+
+econ.click(function() {
+    window.location = 'https://time.com/6244395/sam-bankman-fried-pleads-not-guilty/#:~:text=Sam%20Bankman%2DFried%20leaves%20the,%2C%20on%20January%2003%2C%202023.&text=NEW%20YORK%20%E2%80%94%20FTX%20founder%20Sam,tentative%20trial%20date%20for%20October.';
+})
+
+econo.click(function() {
+    window.location = 'https://slate.com/technology/2023/01/elon-musk-tesla-stock-twitter-china.html';
+})
+
+econom.click(function() {
+    window.location = 'https://invezz.com/news/2023/01/05/should-i-buy-coca-cola-shares-in-january-2023/';
+})
+
+tech.click(function() {
+    window.location = '';
+})
+
+techn.click(function() {
+    window.location = 'https://www.cnet.com/tech/mobile/8-most-impressive-rumored-phones-coming-out-in-2023/';
+})
+
+techno.click(function() {
+    window.location = 'https://arstechnica.com/gadgets/2023/01/nvidia-unveils-a-broad-range-of-efficient-new-laptop-gpus-from-rtx-4050-to-4090/';
+})
+$('#redirect').click(function() {
+    window.location = 'calendarv2.html';
+
+})
+
---
+++ FInished-Project/time.js
@@ -0,0 +1,20 @@
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+function updateDate() {
+    var currentDate = new Date();
+    var date = currentDate.getDate();
+    var month = currentDate.getMonth() + 1; // January is 0
+    var year = currentDate.getFullYear();
+    var day = currentDate.getDay();
+
+    var element = document.getElementById("current_date");
+    var elements = document.getElementById("tech");
+
+
+    // Update the text of the element with the ID "current_date"
+    element.innerHTML = ": " + days[day] + " " + date + "/" + month + "/" + year;
+    elements.innerHTML = "Top tech news for " + days[day] + " " + date + "/" + month + "/" + year;
+  }
+
+  // Update the date every 1 second
+  setInterval(updateDate, 1000);
